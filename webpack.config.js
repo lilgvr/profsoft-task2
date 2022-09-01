0f1b1db3d26be3884a65b026b8546ff5e913473b
lilgvr <71455385+lilgvr@users.noreply.github.com>
2022-09-01 13:18:35 +0400

    initial

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'production',
+    entry: {
+        main: './src/index.js'
+    },
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: '[name][contenthash].js',
+        clean: true
+    },
+    performance: {
+        hints: false
+    },
+    devServer: {
+        port: 3000,
+        hot: true,
+        compress: true,
+        static: {
+            directory: path.join(__dirname, 'build')
+        }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.html$/i,
+                loader: 'raw-loader'
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: "index.html",
+            template: "./src/index.html"
+        })
+    ]
+}
